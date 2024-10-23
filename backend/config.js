42678cd50bb6af166c7fa544aba6de14a67e4d8d
Adeniji ALiu Adeyemi <adenijialiuadeyemi@gmail.com>
2024-10-24 00:00:36 +0100

    updated .gitignore

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDbURL = "mongodb+srv://root:root@product-management.ukpef.mongodb.net/products-collection?retryWrites=true&w=majority&appName=product-management"
